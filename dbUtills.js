e0b42a2643d4f9633561e5b90ddb0331cd773c1a
Sina Kalantar <sina1999.sk@gmail.com>
2022-04-05 14:18:12 -0700

    Add files via upload

---
+++ dbUtills.js
@@ -0,0 +1,12 @@
+'use strict';
+const neo4j = require("neo4j-driver");
+require('dotenv').config()
+
+const driver = neo4j.driver(process.env.DB_URI,neo4j.auth.basic(process.env.DB_USER, process.env.DB_PASSWORD));
+
+exports.getSession = (context) =>{
+    if(!context.neo4jSession){
+        context.neo4jSession = driver.session();
+    }
+    return context.neo4jSession;
+}
