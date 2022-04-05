47670eb04703846357ea340553425b7e796ad34b
Can Huynh <canhuynh1998@gmail.com>
2022-04-04 17:11:26 -0700

    Tested tailblock

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const artistRouter = require("./routes/Artist");
 const concertRouter = require("./routes/Concert")
 const app = express(), api = express();
 
+app.set("view engine", "ejs");
 
 app.use("/api/v1/artist",artistRouter)
 app.use("/api/v1/concert", concertRouter)
@@ -17,3 +18,7 @@ app.listen(3000 , () =>{
 app.get('/', function(req, res) {
     res.send("Hello World")
 })
+
+// app.get('/concert-search', function(req, res) {
+//     res.render("templates/filterConcert")
+// })
