019c6da5a8bf174042e2f7b14ebfe6820ee3c13f
Eunice Oh <euniceohsj@gmail.com>
2022-05-12 16:18:47 -0700

    fixed stuff

--- backend/routes/Person.js
+++ backend/routes/Person.js
@@ -116,13 +116,6 @@ var login = function(session, email, Lpass) {
         });
 };
 
-
-const dbUtils = require('../dbUtils');
-const express = require('express');
-const router = express.Router()
-
-router.use(express.json())
-
 router.get("/getUser", (req, res) => {
     console.log(req.query)
 
